54e3cfa090f4c07026fc472bf4080330618f8e89
borsh357 <alexey.cher357@gmail.com>
2020-09-29 15:31:19 +0300

    solved eslint versions collision

--- src/Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HomePage() {
-  return (
-    <div className="page">
-      <h1>Home Page</h1>
-    </div>
-  );
-}
--- src/Pages/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SettingsPage() {
-  return (
-    <div className="page">
-      <h1>Settings Page</h1>
-    </div>
-  );
-}
